da28b8dcff6b7fdb78fa55207673c82657002e47
alvinmr <maulanaalvin1@gmail.com>
2022-06-29 18:10:00 +0800

    done gebetan agung

--- Web/list gebetan agung/index.php
+++ Web/list gebetan agung/index.php
@@ -1,8 +1,30 @@
-<?php
+<!-- make html to parse data from database.json -->
+<!DOCTYPE html>
+<html lang="en">
 
-$ver = SQLite3::version();
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Ini Semua adalah...</title>
+    </head>
 
-echo $ver['versionString'] . "\n";
-echo $ver['versionNumber'] . "\n";
+    <body>
+        <h1>List Gebetan Agung</h1>
+        <ul></ul>
+    </body>
 
-var_dump($ver);
+    <script>
+    // make script to parse json file in ul tag
+    fetch('./database/database.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = `<a href="detail.php?id=${item.id}">${item.name}</a>`;
+                document.querySelector('ul').appendChild(li);
+            });
+        });
+    </script>
+
+</html>
